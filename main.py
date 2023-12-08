98c4d3a3adb827177dea211201e28a5a201c43d3
28bwainwright <150368532+28bwainwright@users.noreply.github.com>
2023-12-08 16:41:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ st.subheader('Northern Alaska: Fall of 1979')
 
 fig = px.scatter_mapbox(df_longlat, lat='LAT', lon='LON', color='Season', zoom=5, hover_name='Year', hover_data=['Season', 'LAT', 'LON'], color_discrete_sequence=px.colors.qualitative.Light24)
 fig.update_layout(
-    mapbox_style="carto-positron",
+    mapbox_style="white-bg",
     mapbox_layers=[
         {
             "below": 'traces',
@@ -24,6 +24,13 @@ fig.update_layout(
             "source": [
                 "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
             ]
+        },
+        {
+            "sourcetype": "raster",
+            "sourceattribution": "Government of Canada",
+            "source": ["https://geo.weather.gc.ca/geomet/?"
+                       "SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX={bbox-epsg-3857}&CRS=EPSG:3857"
+                       "&WIDTH=1000&HEIGHT=1000&LAYERS=RADAR_1KM_RDBR&TILED=true&FORMAT=image/png"],
         }
       ])
 fig.update_layout(height=1000)
